2a6db1e799a1176cd1fc216ae57a5ca2f4879a26
rezamarammaz-oss <rezamarammaz@gmail.com>
2026-07-06 04:58:33 +0330

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,358 +1,103 @@
-```objectivec
-// ============================================================
-//  A.A.I_PUBGM_v4.4_VIP_FINAL.mm
-//  Target: PUBG Mobile v4.4 iOS
-//  Injection: ESign / Sideloadly / AltStore
-//  Architecture: arm64 Production Build
-//  Anti-Ban: Full Military-Grade Implementation
-// ============================================================
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <CoreGraphics/CoreGraphics.h>
-#import <QuartzCore/QuartzCore.h>
-#import <Metal/Metal.h>
-#import <MetalKit/MetalKit.h>
-#import <AVFoundation/AVFoundation.h>
-#import <ReplayKit/ReplayKit.h>
-#import <Security/Security.h>
 #import <dlfcn.h>
 #import <mach-o/dyld.h>
 #import <mach/mach.h>
-#import <mach/mach_time.h>
 #import <mach/vm_map.h>
 #import <sys/sysctl.h>
-#import <sys/stat.h>
-#import <sys/mman.h>
 #import <sys/socket.h>
 #import <netinet/in.h>
-#import <netinet/tcp.h>
 #import <arpa/inet.h>
-#import <objc/runtime.h>
-#import <objc/message.h>
-#import <pthread.h>
-#import <unistd.h>
-#import <fcntl.h>
-#import <spawn.h>
-#import <dirent.h>
+#import <netdb.h>
 #import <substrate.h>
 
-#pragma mark - Global Settings
+static BOOL g_Aimbot = YES;
+static int g_FOV = 15;
+static int g_Smooth = 12;
 
-static BOOL g_AimbotEnabled = YES;
-static BOOL g_ESPEnabled = YES;
-static BOOL g_NoRecoil = YES;
-static BOOL g_NoSpread = YES;
-static int g_AimbotFOV = 15;
-static int g_AimbotSmooth = 12;
-static int g_AimbotMaxDistance = 200;
-static int g_ESPMaxDistance = 150;
-static BOOL g_AntiBanEnabled = YES;
-static BOOL g_StreamProof = YES;
-
-#pragma mark - UE4 Structures
-
-struct FVector { float X, Y, Z; };
-struct FRotator { float Pitch, Yaw, Roll; };
-struct FMinimalViewInfo {
-    FVector Location;
-    FRotator Rotation;
-    float FOV;
-    float DesiredFOV;
-    float OrthoWidth;
-    float OrthoNearClipPlane;
-    float OrthoFarClipPlane;
-    float AspectRatio;
-};
-
-#pragma mark - Offsets (v4.4)
-
-static uintptr_t g_GWorld = 0;
-static uintptr_t g_GNames = 0;
-static uintptr_t g_ObjectArray = 0;
-static uintptr_t g_ActorCount = 0;
-static FVector g_LocalPlayerPosition;
-static FRotator g_LocalPlayerRotation;
-
-#pragma mark - Anti-Ban Engine
-
-@interface AntiBanEngine : NSObject
-+ (void)initialize;
-+ (void)disableDebuggerChecks;
-+ (void)bypassIntegrityChecks;
-+ (void)blockReportServers;
-+ (void)hideFromMemoryScanners;
-+ (void)encryptMemory;
-+ (void)bypassSSL;
-@end
-
-@implementation AntiBanEngine
-
-+ (void)initialize {
-    if (!g_AntiBanEnabled) return;
-    [self disableDebuggerChecks];
-    [self bypassIntegrityChecks];
-    [self blockReportServers];
-    [self hideFromMemoryScanners];
-    [self encryptMemory];
-    [self bypassSSL];
-}
-
-+ (void)disableDebuggerChecks {
+// ====== ANTI-BAN CORE ======
+static void AntiBan_Setup(void) {
+    // 1. Anti-Debug
     void* ptrace_ptr = dlsym(RTLD_DEFAULT, "ptrace");
     if (ptrace_ptr) {
         MSHookFunction(ptrace_ptr, (void*)^int(int req, pid_t pid, caddr_t addr, int data) {
-            if (req == 31) return 0;
-            return ptrace(req, pid, addr, data);
+            return (req == 31) ? 0 : ptrace(req, pid, addr, data);
         }, NULL);
     }
     
+    // 2. Block report servers
+    void* getaddrinfo_ptr = dlsym(RTLD_DEFAULT, "getaddrinfo");
+    if (getaddrinfo_ptr) {
+        MSHookFunction(getaddrinfo_ptr, (void*)^int(const char* h, const char* s, const struct addrinfo* hints, struct addrinfo** r) {
+            if (h && (strstr(h, "report") || strstr(h, "analytics") || strstr(h, "tracking"))) return EAI_NONAME;
+            return getaddrinfo(h, s, hints, r);
+        }, NULL);
+    }
+    
+    // 3. Hide traced flag
     void* sysctl_ptr = dlsym(RTLD_DEFAULT, "sysctl");
     if (sysctl_ptr) {
-        MSHookFunction(sysctl_ptr, (void*)^int(int* name, u_int namelen, void* oldp, size_t* oldlenp, void* newp, size_t newlen) {
-            int ret = sysctl(name, namelen, oldp, oldlenp, newp, newlen);
-            if (namelen == 4 && name[0] == CTL_KERN && name[1] == KERN_PROC && name[2] == KERN_PROC_PID && name[3] == getpid()) {
+        MSHookFunction(sysctl_ptr, (void*)^int(int* name, u_int nl, void* oldp, size_t* ol, void* np, size_t nl2) {
+            int r = sysctl(name, nl, oldp, ol, np, nl2);
+            if (nl == 4 && name[0] == CTL_KERN && name[1] == KERN_PROC && name[2] == KERN_PROC_PID && name[3] == getpid()) {
                 struct kinfo_proc* info = (struct kinfo_proc*)oldp;
                 if (info) info->kp_proc.p_flag &= ~0x800;
             }
-            return ret;
+            return r;
         }, NULL);
     }
 }
 
-+ (void)bypassIntegrityChecks {
-    void* vm_region_ptr = dlsym(RTLD_DEFAULT, "mach_vm_region_recurse");
-    if (vm_region_ptr) {
-        MSHookFunction(vm_region_ptr, (void*)^kern_return_t(vm_map_t map, vm_address_t* addr, vm_size_t* size, vm_region_recurse_info_t info, mach_msg_type_number_t* infoCnt) {
-            kern_return_t kr = mach_vm_region_recurse(map, addr, size, info, infoCnt);
-            if (kr == KERN_SUCCESS && info) {
-                info->protection = VM_PROT_READ | VM_PROT_EXECUTE;
-            }
-            return kr;
-        }, NULL);
-    }
+// ====== AIMBOT ======
+struct FVector { float X, Y, Z; };
+struct FRotator { float Pitch, Yaw, Roll; };
+
+static FVector CalcAngle(FVector src, FVector dst) {
+    FVector d = {dst.X - src.X, dst.Y - src.Y, dst.Z - src.Z};
+    float h = sqrtf(d.X*d.X + d.Y*d.Y);
+    return {-atan2f(d.Z, h) * 57.2957795f, atan2f(d.Y, d.X) * 57.2957795f, 0};
 }
 
-+ (void)blockReportServers {
-    void* getaddrinfo_ptr = dlsym(RTLD_DEFAULT, "getaddrinfo");
-    if (getaddrinfo_ptr) {
-        MSHookFunction(getaddrinfo_ptr, (void*)^int(const char* host, const char* serv, const struct addrinfo* hints, struct addrinfo** res) {
-            if (host && (strstr(host, "report") || strstr(host, "analytics") || strstr(host, "securitycheck"))) {
-                return EAI_NONAME;
-            }
-            return getaddrinfo(host, serv, hints, res);
-        }, NULL);
-    }
-}
+// ====== ESP OVERLAY ======
+static UIView* g_Overlay = nil;
 
-+ (void)hideFromMemoryScanners {
-    Dl_info info;
-    dladdr((void*)&AntiBanEngine::initialize, &info);
-    struct mach_header_64* header = (struct mach_header_64*)info.dli_fbase;
-    vm_protect(mach_task_self(), (vm_address_t)header, sizeof(struct mach_header_64), false, VM_PROT_READ | VM_PROT_WRITE);
-    header->flags |= 0x80000000;
-    vm_protect(mach_task_self(), (vm_address_t)header, sizeof(struct mach_header_64), false, VM_PROT_READ | VM_PROT_EXECUTE);
-}
-
-+ (void)encryptMemory {
-    uint64_t key = mach_absolute_time();
-    Dl_info info;
-    dladdr((void*)&AntiBanEngine::initialize, &info);
-    uintptr_t base = (uintptr_t)info.dli_fbase;
-    vm_protect(mach_task_self(), base, 0x10000, false, VM_PROT_READ | VM_PROT_WRITE);
-    for (int i = 0; i < 0x1000; i++) {
-        ((uint64_t*)(base))[i] ^= key;
-    }
-    vm_protect(mach_task_self(), base, 0x10000, false, VM_PROT_READ | VM_PROT_EXECUTE);
-}
-
-+ (void)bypassSSL {
-    void* secTrustEval = dlsym(RTLD_DEFAULT, "SecTrustEvaluate");
-    if (secTrustEval) {
-        MSHookFunction(secTrustEval, (void*)^OSStatus(SecTrustRef trust, SecTrustResultType* result) {
-            if (result) *result = kSecTrustResultProceed;
-            return errSecSuccess;
-        }, NULL);
-    }
-}
-
-@end
-
-#pragma mark - Aimbot Engine
-
-@interface AimbotEngine : NSObject
-+ (void)initialize;
-+ (void)processAimbot:(FRotator*)viewRotation;
-@end
-
-@implementation AimbotEngine
-
-static void* currentTarget = NULL;
-static uint64_t lastAimTime = 0;
-
-+ (void)initialize {
-    if (!g_AimbotEnabled) return;
-}
-
-+ (void)processAimbot:(FRotator*)viewRotation {
-    if (!g_AimbotEnabled) return;
-    
-    uint64_t now = mach_absolute_time();
-    if (now - lastAimTime < 10000000) return;
-    lastAimTime = now;
-    
-    void* target = [self findBestTarget];
-    if (!target) return;
-    
-    FVector targetPos = [self getBonePosition:target bone:5];
-    FVector aimAngle = [self calcAngle:g_LocalPlayerPosition to:targetPos];
-    
-    float smooth = g_AimbotSmooth / 100.0f;
-    viewRotation->Pitch += (aimAngle.X - viewRotation->Pitch) * smooth;
-    viewRotation->Yaw += (aimAngle.Y - viewRotation->Yaw) * smooth;
-    
-    while (viewRotation->Yaw > 180.0f) viewRotation->Yaw -= 360.0f;
-    while (viewRotation->Yaw < -180.0f) viewRotation->Yaw += 360.0f;
-}
-
-+ (void*)findBestTarget {
-    return NULL;
-}
-
-+ (FVector)getBonePosition:(void*)actor bone:(int)boneID {
-    FVector pos = {0, 0, 0};
-    void* mesh = *(void**)((uintptr_t)actor + 0x420);
-    if (mesh) {
-        pos = *(FVector*)((uintptr_t)mesh + 0x140 + boneID * 0x30);
-    }
-    return pos;
-}
-
-+ (FVector)calcAngle:(FVector)src to:(FVector)dst {
-    FVector delta = {dst.X - src.X, dst.Y - src.Y, dst.Z - src.Z};
-    float hyp = sqrtf(delta.X * delta.X + delta.Y * delta.Y);
-    FVector angle;
-    angle.Y = atan2f(delta.Y, delta.X) * 57.2957795f;
-    angle.X = -atan2f(delta.Z, hyp) * 57.2957795f;
-    angle.Z = 0.0f;
-    return angle;
-}
-
-@end
-
-#pragma mark - ESP Engine
-
-@interface ESPEngine : NSObject
-+ (void)initialize;
-+ (void)drawESP;
-@end
-
-@implementation ESPEngine
-
-static UIView* overlayView = nil;
-static CADisplayLink* renderLink = nil;
-
-+ (void)initialize {
-    if (!g_ESPEnabled) return;
-    
+static void ESP_Setup(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIWindow* window = [UIApplication sharedApplication].keyWindow;
-        if (window) {
-            overlayView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
-            overlayView.backgroundColor = [UIColor clearColor];
-            overlayView.userInteractionEnabled = NO;
-            overlayView.layer.zPosition = FLT_MAX;
-            
-            if (g_StreamProof) {
-                CAMetalLayer* metal = [CAMetalLayer layer];
-                metal.frame = overlayView.bounds;
-                metal.device = MTLCreateSystemDefaultDevice();
-                metal.pixelFormat = MTLPixelFormatBGRA8Unorm;
-                metal.framebufferOnly = NO;
-                metal.opaque = NO;
-                [overlayView.layer addSublayer:metal];
+        UIWindow* w = nil;
+        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
+            if (scene.activationState == UISceneActivationStateForegroundActive) {
+                w = scene.windows.firstObject;
+                break;
             }
-            
-            [window addSubview:overlayView];
-            
-            renderLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(drawESP)];
-            renderLink.preferredFramesPerSecond = 60;
-            [renderLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+        }
+        if (w) {
+            g_Overlay = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+            g_Overlay.backgroundColor = [UIColor clearColor];
+            g_Overlay.userInteractionEnabled = NO;
+            g_Overlay.layer.zPosition = FLT_MAX;
+            [w addSubview:g_Overlay];
         }
     });
 }
 
-+ (void)drawESP {
-    // ESP rendering logic
-}
-
-@end
-
-#pragma mark - Visual Mods
-
-@interface VisualMods : NSObject
-+ (void)initialize;
-@end
-
-@implementation VisualMods
-
-+ (void)initialize {
-    if (g_NoRecoil) {
-        // Hook weapon recoil
-    }
-    if (g_NoSpread) {
-        // Hook weapon spread
-    }
-}
-
-@end
-
-#pragma mark - Lag Fix
-
-@interface LagFix : NSObject
-+ (void)initialize;
-@end
-
-@implementation LagFix
-
-+ (void)initialize {
+// ====== LAG FIX ======
+static void LagFix_Setup(void) {
     setpriority(PRIO_PROCESS, getpid(), -20);
-    pthread_set_qos_class_self_np(QOS_CLASS_USER_INTERACTIVE, 0);
-    
     setenv("PUBGM_SHADOW_QUALITY", "0", 1);
     setenv("PUBGM_POST_PROCESS", "0", 1);
-    setenv("PUBGM_MOTION_BLUR", "0", 1);
-    setenv("PUBGM_VSYNC", "0", 1);
-    
-    int bufferSize = 524288;
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &bufferSize, sizeof(bufferSize));
-    setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &bufferSize, sizeof(bufferSize));
-    close(sock);
+    int bs = 524288;
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(s, SOL_SOCKET, SO_RCVBUF, &bs, sizeof(bs));
+    setsockopt(s, SOL_SOCKET, SO_SNDBUF, &bs, sizeof(bs));
+    close(s);
 }
 
-@end
-
-#pragma mark - Constructor
-
+// ====== LAUNCH ======
 __attribute__((constructor))
-static void AAIPUBG_Init() {
-    @autoreleasepool {
-        [AntiBanEngine initialize];
-        [LagFix initialize];
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-            [AimbotEngine initialize];
-            [ESPEngine initialize];
-            [VisualMods initialize];
-        });
-    }
+static void Init(void) {
+    AntiBan_Setup();
+    LagFix_Setup();
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        ESP_Setup();
+    });
 }
-
-__attribute__((destructor))
-static void AAIPUBG_Cleanup() {}
-
-// ============================================================
-//  END OF VIP dylib
-// ============================================================
-```
