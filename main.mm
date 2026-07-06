c0b1adbb7e1b533290840ec39ea97a3529cc8575
rezamarammaz-oss <rezamarammaz@gmail.com>
2026-07-06 05:08:19 +0330

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,3 +1,4 @@
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <dlfcn.h>
@@ -8,52 +9,45 @@
 #import <netdb.h>
 #import <substrate.h>
 
-static BOOL g_Aimbot = YES;
-static int g_FOV = 15;
-static int g_Smooth = 12;
+ int (*orig_ptrace)(int, pid_t, caddr_t, int) = NULL;
+ int (*orig_getaddrinfo)( char*,  char*,   addrinfo*,  addrinfo**) = NULL;
+ int (*orig_sysctl)(int*, u_int, void*, size_t*, void*, size_t) = NULL;
 
-// ====== ANTI-BAN CORE ======
-static int (*orig_ptrace)(int, pid_t, caddr_t, int) = NULL;
-
-static void AntiBan_Setup(void) {
-    // 1. Anti-Debug: hook ptrace
-    orig_ptrace = (int(*)(int, pid_t, caddr_t, int))dlsym(RTLD_DEFAULT, "ptrace");
-    if (orig_ptrace) {
-        MSHookFunction((void*)orig_ptrace, (void*)^int(int req, pid_t pid, caddr_t addr, int data) {
-            return (req == 31) ? 0 : orig_ptrace(req, pid, addr, data);
+ void _Setup(void) {
+    orig_ptrace = dlsym(RTLD_DEFAULT, "ptrace");
+     (orig_ptrace) {
+        MSHookFunction(orig_ptrace, (void*)^int(int req, pid_t pid, caddr_t addr, int data) {
+             (req == 31)  0;
+             orig_ptrace(req, pid, addr, data);
         }, NULL);
     }
-    
-    // 2. Block report servers
-    int (*orig_getaddrinfo)(const char*, const char*, const struct addrinfo*, struct addrinfo**);
-    orig_getaddrinfo = (int(*)(const char*, const char*, const struct addrinfo*, struct addrinfo**))dlsym(RTLD_DEFAULT, "getaddrinfo");
-    if (orig_getaddrinfo) {
-        MSHookFunction((void*)orig_getaddrinfo, (void*)^int(const char* h, const char* s, const struct addrinfo* hints, struct addrinfo** r) {
-            if (h && (strstr(h, "report") || strstr(h, "analytics") || strstr(h, "tracking"))) return EAI_NONAME;
-            return orig_getaddrinfo(h, s, hints, r);
+
+    orig_getaddrinfo = dlsym(RTLD_DEFAULT, "getaddrinfo");
+     (orig_getaddrinfo) {
+        MSHookFunction(orig_getaddrinfo, (void*)^int( char* host,  char* serv,   addrinfo* hints,  addrinfo** res) {
+             (host && (strstr(host, "report") || strstr(host, "analytics") || strstr(host, "tracking")))
+                 EAI_NONAME;
+             orig_getaddrinfo(host, serv, hints, res);
         }, NULL);
     }
-    
-    // 3. Hide traced flag
-    int (*orig_sysctl)(int*, u_int, void*, size_t*, void*, size_t);
-    orig_sysctl = (int(*)(int*, u_int, void*, size_t*, void*, size_t))dlsym(RTLD_DEFAULT, "sysctl");
-    if (orig_sysctl) {
-        MSHookFunction((void*)orig_sysctl, (void*)^int(int* name, u_int nl, void* oldp, size_t* ol, void* np, size_t nl2) {
-            int r = orig_sysctl(name, nl, oldp, ol, np, nl2);
-            if (nl == 4 && name[0] == CTL_KERN && name[1] == KERN_PROC && name[2] == KERN_PROC_PID && name[3] == getpid()) {
-                struct kinfo_proc* info = (struct kinfo_proc*)oldp;
-                if (info) info->kp_proc.p_flag &= ~0x800;
+
+    orig_sysctl = dlsym(RTLD_DEFAULT, "sysctl");
+     (orig_sysctl) {
+        MSHookFunction(orig_sysctl, (void*)^int(int* name, u_int namelen, void* oldp, size_t* oldlenp, void* newp, size_t newlen) {
+            int ret = orig_sysctl(name, namelen, oldp, oldlenp, newp, newlen);
+             (namelen == 4 && name[0] == CTL_KERN && name[1] == KERN_PROC && name[2] == KERN_PROC_PID && name[3] == getpid()) {
+                 kinfo_proc* info = (_*)oldp;
+                 (info) info->kp_proc.p_flag &= ~0x800;
             }
-            return r;
+             ret;
         }, NULL);
     }
 }
 
-// ====== AIMBOT ======
-struct FVector { float X, Y, Z; };
-struct FRotator { float Pitch, Yaw, Roll; };
+ FVector { float X, Y, Z; };
+ FRotator { float Pitch, Yaw, Roll; };
 
-static FVector CalcAngle(FVector src, FVector dst) {
+FVector (FVector src, FVector dst) {
     FVector ang;
     float dx = dst.X - src.X;
     float dy = dst.Y - src.Y;
@@ -62,22 +56,21 @@ static FVector CalcAngle(FVector src, FVector dst) {
     ang.X = -atan2f(dz, hyp) * 57.2957795f;
     ang.Y = atan2f(dy, dx) * 57.2957795f;
     ang.Z = 0;
-    return ang;
+     ang;
 }
 
-// ====== ESP OVERLAY ======
-static UIView* g_Overlay = nil;
+ UIView* g_Overlay = nil;
 
-static void ESP_Setup(void) {
+ void (void) {
     dispatch_async(dispatch_get_main_queue(), ^{
         UIWindow* w = nil;
-        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
+         (UIWindowScene* scene  [UIApplication sharedApplication].connectedScenes) {
+             (scene.activationState == UISceneActivationStateForegroundActive) {
                 w = scene.windows.firstObject;
-                break;
+                ;
             }
         }
-        if (w) {
+         (w) {
             g_Overlay = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
             g_Overlay.backgroundColor = [UIColor clearColor];
             g_Overlay.userInteractionEnabled = NO;
@@ -87,21 +80,20 @@ static void ESP_Setup(void) {
     });
 }
 
-// ====== LAG FIX ======
-void (void) {
+ void (void) {
     setpriority(PRIO_PROCESS, getpid(), -20);
     setenv("PUBGM_SHADOW_QUALITY", "0", 1);
     setenv("PUBGM_POST_PROCESS", "0", 1);
     int bs = 524288;
     int s = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(s, SOL_SOCKET, SO_RCVBUF, &bs, sizeof(bs));
-    setsockopt(s, SOL_SOCKET, SO_SNDBUF, &bs, sizeof(bs));
+    setsockopt(s, SOL_SOCKET, SO_RCVBUF, &bs, (bs));
+    setsockopt(s, SOL_SOCKET, SO_SNDBUF, &bs, 
+               (bs));
     close(s);
 }
 
-// ====== LAUNCH ======
 __attribute__((constructor))
-stati void Init(void) {
+ void (void) {
     AntiBan_Setup();
     LagFix_Setup();
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
