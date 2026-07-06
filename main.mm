cd07f9c4e2b2926588bb96d47889b8a280b92e50
rezamarammaz-oss <rezamarammaz@gmail.com>
2026-07-06 05:20:18 +0330

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,14 +1,2 @@
-#import <substrate.h>
-#import <dlfcn.h>
-#import <sys/sysctl.h>
-
 __attribute__((constructor))
-static void init_func() {
-    void *ptrace_ptr = dlsym(RTLD_DEFAULT, "ptrace");
-    if (ptrace_ptr) {
-        MSHookFunction(ptrace_ptr, (void *)(int (*)(int, pid_t, caddr_t, int))^int(int req, pid_t a, caddr_t b, int c) {
-            return (req == 31) ? 0 : 0;
-        }, NULL);
-    }
-    setpriority(PRIO_PROCESS, getpid(), -20);
-}
+void test(void) {}
